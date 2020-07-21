62257c307851a22af7f321dde619b68acf07396b
kiril24us <kiril24us@users.noreply.github.com>
2020-07-22 01:28:13 +0300

    Improve code quality - adding classes, comments

---
+++ Green vs Red/Matrix.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Green_vs_Red
+{
+    /// <summary>
+    /// The class Matrix contains all functionality code
+    /// </summary>
+    public class Matrix
+    {
+        private char[,] firstMatrix;
+        private char[,] secondMatrix;
+        private int counter;
+        private int calculatingGreenWantedCell;
+        private int rowCoordinate;
+        private int colCoordinate;
+        private int turns;
+        private int rows;
+        private int cols;
+
+        public Matrix(int[] sizeOfTheMatrix)
+        {
+            rows = sizeOfTheMatrix[1];
+            cols = sizeOfTheMatrix[0];
+            firstMatrix = new char[rows, cols];
+            secondMatrix = new char[rows, cols];
+            counter = 1;
+            calculatingGreenWantedCell = 0;
+        }
+
+        public int Run(int[] coordinatesAndTurns)
+        {
+            turns = coordinatesAndTurns[2];
+            rowCoordinate = coordinatesAndTurns[1];
+            colCoordinate = coordinatesAndTurns[0];
+
+            // Checking the matrix for a green cell for the first time
+            if (firstMatrix[rowCoordinate, colCoordinate] == 'G')
+            {
+                // Parameter for counting the wanted green cell
+                calculatingGreenWantedCell++;
+            }
+
+            // Making second matrix with the same values
+            FillingTheSecondMatrix();
+
+            while (counter != turns)
+            {
+                for (int row = 0; row < firstMatrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < firstMatrix.GetLength(1); col++)
+                    {
+                        int counterGreenNeighbour = 0;
+                        /* Checking all neigbours
+                           for green cells*/
+                        bool IsThereDown = DownNeighbour(row, col, ref counterGreenNeighbour);
+                        bool IsThereRight = RightNeighbour(row, col, ref counterGreenNeighbour);
+                        if (IsThereDown && IsThereRight)
+                        {
+                            RightDownDiagonalNeighbour(row, col, ref counterGreenNeighbour);
+                        }
+                        bool IsThereLeft = LeftNeighbour(row, col, ref counterGreenNeighbour);
+                        if (IsThereDown && IsThereLeft)
+                        {
+                            LeftDownDiagonalNeighbour(row, col, ref counterGreenNeighbour);
+                        }
+                        bool IsThereUp = UpNeighbour(row, col, ref counterGreenNeighbour);
+                        if (IsThereLeft && IsThereUp)
+                        {
+                            LeftUpDiagonalNeighbour(row, col, ref counterGreenNeighbour);
+                        }
+                        if (IsThereRight && IsThereUp)
+                        {
+                            RightUpDiagonalNeighbour(row, col, ref counterGreenNeighbour);
+                        }
+                        // Checking if the cell changes the colour
+                        ValidateCurrentCell(row, col, ref counterGreenNeighbour);
+                    }
+                }
+                counter++;
+                if (secondMatrix[rowCoordinate, colCoordinate] == 'G')
+                {
+                    // If the cell is green, increasing the counter
+                    calculatingGreenWantedCell++;
+                }
+                // coping values from second matrix to first matrix
+                CopyingTwoMatrixes();
+            }
+            return calculatingGreenWantedCell;
+        }
+
+        public void FillingTheFirstMatrix(Func<string> inputRowsMatrix)
+        {
+            for (int row = 0; row < firstMatrix.GetLength(0); row++)
+            {
+                string inputRowMatrix = inputRowsMatrix();
+                for (int col = 0; col < firstMatrix.GetLength(1); col++)
+                {
+                    if (inputRowMatrix.Length == firstMatrix.GetLength(1))
+                    {
+                        if (inputRowMatrix[col] == '1')
+                        {
+                            firstMatrix[row, col] = 'G';
+                        }
+                        if (inputRowMatrix[col] == '0')
+                        {
+                            firstMatrix[row, col] = 'R';
+                        }
+                    }
+                }
+            }
+        }
+
+        public void FillingTheSecondMatrix()
+        {
+            for (int row = 0; row < firstMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < firstMatrix.GetLength(1); col++)
+                {
+                    secondMatrix[row, col] = firstMatrix[row, col];
+                }
+            }
+        }
+
+        public bool DownNeighbour(int row, int col, ref int counterGreenNeighbour)
+        {
+            if (row < firstMatrix.GetLength(0) - 1)
+            {
+                if (firstMatrix[row + 1, col] == 'G')
+                {
+                    counterGreenNeighbour++;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public bool RightNeighbour(int row, int col, ref int counterGreenNeighbour)
+        {
+            if (col < firstMatrix.GetLength(1) - 1)
+            {
+                if (firstMatrix[row, col + 1] == 'G')
+                {
+                    counterGreenNeighbour++;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public void RightDownDiagonalNeighbour(int row, int col, ref int counterGreenNeighbour)
+        {
+            if (firstMatrix[row + 1, col + 1] == 'G')
+            {
+                counterGreenNeighbour++;
+            }
+        }
+
+        public bool LeftNeighbour(int row, int col, ref int counterGreenNeighbour)
+        {
+            if (col > 0)
+            {
+                if (firstMatrix[row, col - 1] == 'G')
+                {
+                    counterGreenNeighbour++;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public void LeftDownDiagonalNeighbour(int row, int col, ref int counterGreenNeighbour)
+        {
+            if (firstMatrix[row + 1, col - 1] == 'G')
+            {
+                counterGreenNeighbour++;
+            }
+        }
+
+        public bool UpNeighbour(int row, int col, ref int counterGreenNeighbour)
+        {
+            if (row > 0)
+            {
+                if (firstMatrix[row - 1, col] == 'G')
+                {
+                    counterGreenNeighbour++;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public void LeftUpDiagonalNeighbour(int row, int col, ref int counterGreenNeighbour)
+        {
+            if (firstMatrix[row - 1, col - 1] == 'G')
+            {
+                counterGreenNeighbour++;
+            }
+        }
+
+        public void RightUpDiagonalNeighbour(int row, int col, ref int counterGreenNeighbour)
+        {
+            if (firstMatrix[row - 1, col + 1] == 'G')
+            {
+                counterGreenNeighbour++;
+            }
+        }
+
+        public void CopyingTwoMatrixes()
+        {
+            for (int row = 0; row < firstMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < firstMatrix.GetLength(1); col++)
+                {
+                    firstMatrix[row, col] = secondMatrix[row, col];
+                }
+            }
+        }
+
+        public void ValidateCurrentCell(int row, int col, ref int counterGreenNeighbour)
+        {
+            if (firstMatrix[row, col] == 'R')
+            {
+                if (counterGreenNeighbour == 3 || counterGreenNeighbour == 6)
+                {
+                    secondMatrix[row, col] = 'G';
+                }
+            }
+            if (firstMatrix[row, col] == 'G')
+            {
+                if (counterGreenNeighbour == 0 || counterGreenNeighbour == 1 ||
+                    counterGreenNeighbour == 4 || counterGreenNeighbour == 5 ||
+                    counterGreenNeighbour == 7 || counterGreenNeighbour == 8)
+                {
+                    secondMatrix[row, col] = 'R';
+                }
+            }
+            counterGreenNeighbour = 0;
+        }
+    }
+}
--- Green vs Red/Program.cs
+++ Green vs Red/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.Tracing;
+using System.Drawing;
 using System.Dynamic;
 using System.Linq;
 
@@ -9,232 +10,57 @@ namespace Green_vs_Red
     {
         static void Main()
         {
-            int rows = 0;
-            int cols = 0;
-
-            int[] sizeOfTheMatrix = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries)
-                                                      .Select(int.Parse)
-                                                      .ToArray();
-
-            if (sizeOfTheMatrix.Length == 2 && sizeOfTheMatrix[1] > 0 &&
-                sizeOfTheMatrix[1] < 1000 && sizeOfTheMatrix[0] > 0
-                && sizeOfTheMatrix[0] <= sizeOfTheMatrix[1])
+            try
             {
-                rows = sizeOfTheMatrix[1];
-                cols = sizeOfTheMatrix[0];
-            }
-
-            char[,] matrix = new char[rows, cols];
-            char[,] secondMatrix = new char[rows, cols];
-            FillingTheMatrix(matrix);
-            FillingTheSecondMatrix(matrix, secondMatrix);
-            int[] coordinatesAndTurns = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries)
-                                                               .Select(int.Parse)
-                                                               .ToArray();
-            int rowCoordinate = 0;
-            int colCoordinate = 0;
-            int turns = 0;
-            if (coordinatesAndTurns[0] <= matrix.GetLength(1) - 1 &&
-                coordinatesAndTurns[1] <= matrix.GetLength(0) - 1)
-            {
-                turns = coordinatesAndTurns[2];
-                rowCoordinate = coordinatesAndTurns[1];
-                colCoordinate = coordinatesAndTurns[0];
-            }
-
-            int counter = 1;
-            int calculatingGreenWantedCell = 0;
-            if (matrix[rowCoordinate, colCoordinate] == 'G')
-            {
-                calculatingGreenWantedCell++;
-            }
-            while (counter != turns)
-            {                            
-                for (int row = 0; row < matrix.GetLength(0); row++)
+                // Reading the size of the matrix
+                int[] sizeOfTheMatrix = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries)
+                                                          .Select(int.Parse)
+                                                          .ToArray();
+                Matrix matrix;
+                if (sizeOfTheMatrix.Length == 2 && sizeOfTheMatrix[1] > 0 &&
+                    sizeOfTheMatrix[1] < 1000 && sizeOfTheMatrix[0] > 0
+                    && sizeOfTheMatrix[0] <= sizeOfTheMatrix[1])
                 {
-                    for (int col = 0; col < matrix.GetLength(1); col++)
-                    {
-                        int counterGreenNeighbour = 0;
-                        bool IsThereDown = DownNeighbour(matrix, row, col, ref counterGreenNeighbour);
-                        bool IsThereRight = RightNeighbour(matrix, row, col, ref counterGreenNeighbour);
-                        if (IsThereDown && IsThereRight)
-                        {
-                            RightDownDiagonalNeighbour(matrix, row, col, ref counterGreenNeighbour);
-                        }
-                        bool IsThereLeft = LeftNeighbour(matrix, row, col, ref counterGreenNeighbour);
-                        if (IsThereDown && IsThereLeft)
-                        {
-                            LeftDownDiagonalNeighbour(matrix, row, col, ref counterGreenNeighbour);
-                        }
-                        bool IsThereUp = UpNeighbour(matrix, row, col, ref counterGreenNeighbour);
-                        if (IsThereLeft && IsThereUp)
-                        {
-                            LeftUpDiagonalNeighbour(matrix, row, col, ref counterGreenNeighbour);
-                        }
-                        if (IsThereRight && IsThereUp)
-                        {
-                            RightUpDiagonalNeighbour(matrix, row, col, ref counterGreenNeighbour);
-                        }
-                        ValidateCurrentCell(matrix, secondMatrix, row, col, ref counterGreenNeighbour, ref counter);                        
-                    }
+                    matrix = new Matrix(sizeOfTheMatrix);
                 }
-                counter++;
-                if (secondMatrix[rowCoordinate, colCoordinate] == 'G')
+                else
                 {
-                    calculatingGreenWantedCell++;
+                    throw new Exception("Invalid input!");
                 }
-                CopyingTwoMatrixes(matrix, secondMatrix);
-            }
-            Console.WriteLine(calculatingGreenWantedCell);
-        }
 
-        private static void CopyingTwoMatrixes(char[,] matrix, char[,] secondMatrix)
-        {
-            for (int row = 0; row < matrix.GetLength(0); row++)
-            {
-                for (int col = 0; col < matrix.GetLength(1); col++)
+                // Reading the input and filling the cells in the matrix with colours
+                matrix.FillingTheFirstMatrix(Console.ReadLine);
+
+                // Reading the coordinates with the wanted cell and turns
+                int[] coordinatesAndTurns = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries)
+                                                                   .Select(int.Parse)
+                                                                   .ToArray();
+                int calculatingGreenWantedCell = 0;
+                if (coordinatesAndTurns.Length == 3 &&
+                    coordinatesAndTurns[0] < sizeOfTheMatrix[0] &&
+                    coordinatesAndTurns[1] < sizeOfTheMatrix[1])
                 {
-                    matrix[row, col] = secondMatrix[row, col];
+                    calculatingGreenWantedCell = matrix.Run(coordinatesAndTurns);
                 }
-            }
-        }
-
-        private static void FillingTheSecondMatrix(char[,] matrix, char[,] secondMatrix)
-        {
-            for (int row = 0; row < matrix.GetLength(0); row++)
-            {                
-                for (int col = 0; col < matrix.GetLength(1); col++)
+                else
                 {
-                    secondMatrix[row, col] = matrix[row, col];
+                    throw new Exception("Invalid input!");
                 }
-            }
-        }
 
-        private static void ValidateCurrentCell(char[,] matrix, char[,] secondMatrix, int row, int col, ref int counterGreenNeighbour, ref int counter)
-        {
-            if (matrix[row, col] == 'R')
-            {
-                if (counterGreenNeighbour == 3 || counterGreenNeighbour == 6)
-                {
-                    secondMatrix[row, col] = 'G';
-                }
+                /* The second example with 4x4 grid
+                   the expected result is 14...
+                   I think that it must be some kind of misunderstanding
+                   because the first two turns, the cell with coordinations 2-2
+                   remains red, after that becomes green and for the rest of the turns remains
+                   green, so 15 turns - 2 turns(staying red) = 13 turns for green.
+                 */
+                Console.WriteLine(calculatingGreenWantedCell);
             }
-            if (matrix[row, col] == 'G')
+            catch (Exception ex)
             {
-                if (counterGreenNeighbour == 0 || counterGreenNeighbour == 1 ||
-                    counterGreenNeighbour == 4 || counterGreenNeighbour == 5 ||
-                    counterGreenNeighbour == 7 || counterGreenNeighbour == 8)
-                {
-                    secondMatrix[row, col] = 'R';
-                }
+                Console.WriteLine(ex.Message);
             }
-            counterGreenNeighbour = 0;           
-        }
 
-        private static void RightUpDiagonalNeighbour(char[,] matrix, int row, int col, ref int counterGreenNeighbour)
-        {
-            if (matrix[row - 1, col + 1] == 'G')
-            {
-                counterGreenNeighbour++;
-            }
-        }
-
-        private static void LeftUpDiagonalNeighbour(char[,] matrix, int row, int col, ref int counterGreenNeighbour)
-        {
-            if (matrix[row - 1, col - 1] == 'G')
-            {
-                counterGreenNeighbour++;
-            }
-        }
-
-        private static bool UpNeighbour(char[,] matrix, int row, int col, ref int counterGreenNeighbour)
-        {
-            if (row > 0)
-            {
-                if (matrix[row - 1, col] == 'G')
-                {
-                    counterGreenNeighbour++;
-                }
-                return true;
-            }
-            return false;
-        }
-
-        private static void LeftDownDiagonalNeighbour(char[,] matrix, int row, int col, ref int counterGreenNeighbour)
-        {
-            if (matrix[row + 1, col - 1] == 'G')
-            {
-                counterGreenNeighbour++;
-            }
-        }
-
-        private static bool LeftNeighbour(char[,] matrix, int row, int col, ref int counterGreenNeighbour)
-        {
-            if (col > 0)
-            {
-                if (matrix[row, col - 1] == 'G')
-                {
-                    counterGreenNeighbour++;
-                }
-                return true;
-            }
-            return false;
-        }
-
-        private static void RightDownDiagonalNeighbour(char[,] matrix, int row, int col, ref int counterGreenNeighbour)
-        {
-            if (matrix[row + 1, col + 1] == 'G')
-            {
-                counterGreenNeighbour++;
-            }
-        }
-
-        private static bool RightNeighbour(char[,] matrix, int row, int col, ref int counterGreenNeighbour)
-        {
-            if (col < matrix.GetLength(1) - 1)
-            {
-                if (matrix[row, col + 1] == 'G')
-                {
-                    counterGreenNeighbour++;
-                }
-                return true;
-            }
-            return false;
-        }
-
-        private static bool DownNeighbour(char[,] matrix, int row, int col, ref int counterGreenNeighbour)
-        {
-            if (row < matrix.GetLength(0) - 1)
-            {                
-                if (matrix[row + 1, col] == 'G')
-                {
-                    counterGreenNeighbour++;
-                }
-                return true;
-            }
-            return false;
-        }
-
-        private static void FillingTheMatrix(char[,] matrix)
-        {
-            for (int row = 0; row < matrix.GetLength(0); row++)
-            {
-                string inputRowMatrix = Console.ReadLine();
-                for (int col = 0; col < matrix.GetLength(1); col++)
-                {
-                    if (inputRowMatrix.Length == matrix.GetLength(1))
-                    {
-                        if (inputRowMatrix[col] == '1')
-                        {
-                            matrix[row, col] = 'G';
-                        }
-                        if (inputRowMatrix[col] == '0')
-                        {
-                            matrix[row, col] = 'R';
-                        }
-                    }
-                }
-            }
         }
     }
 }
